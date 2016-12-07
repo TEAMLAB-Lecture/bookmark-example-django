24e9281e4e7f87ba47114ed2db38fe99826b72be
Sungchul Choi <sc82.choi@gachon.ac.kr>
2016-12-08 00:04:17 +0900

    Add session examples 2

--- bookmark/views.py
+++ bookmark/views.py
@@ -4,41 +4,38 @@ from bookmark.models import Bookmark
 
 import datetime
 
-def current_datetime(request):
-    now = datetime.datetime.now()
-    return render(request, 'hello.html', {'time': now})
-
-def hello(request, user_name):
-    spams = ['spam ' + user_name, 'Spam ' + user_name, 'SPAM! ' + user_name]
-    return render(request, 'spams.html', {'spams': spams})
 
 def index(request):
-    return render(request, 'index.html', {"project_name": "TeamLab"})
+    email = request.session['login_id']
+    return render(request, 'index.html', {"login_id": email})
 
 def bookmark(request):
+    email = request.session['login_id']
     all_bookmark = None
     if request.method == 'POST':
         bookmark = Bookmark(bookmark_name=request.POST["bookmark_name"], bookmark_url=request.POST["bookmark_url"])
         bookmark.save()
         all_bookmark = Bookmark.objects.all()
 
-        result = {"all_bookmark": all_bookmark,
+        result = {"login_id": email,
+                    "all_bookmark": all_bookmark,
                   "bookmark_name": request.POST["bookmark_name"],
                   "bookmark_url": request.POST["bookmark_url"]}
 
         return render(request, 'bookmark.html', result )
     else:
         all_bookmark = Bookmark.objects.all()
-        result = {"all_bookmark" : all_bookmark}
+        result = {"login_id": email,
+            "all_bookmark" : all_bookmark}
         return render(request, 'bookmark.html', result )
 
 def login(request):
-    return render(request, 'login_form.html')
+    email = request.session['login_id']
+    return render(request, 'login_form.html', {"login_id": email})
 
 def check_login(request):
     if request.method == 'POST':
         print("Hello Login")
         email = request.POST["email"]
-        print(email)
         request.session['login_id'] = email
         return render(request, 'index.html', {"login_id": email})
